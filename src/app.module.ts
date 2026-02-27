12d860e5fb9a8eeb0f72d197a951cda8e0888bf4
Shikhar Negi <shikharnegi31@gmail.com>
2026-02-27 15:30:04 +0530

    Added authorization(return token)part1

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JobsModule } from './jobs/jobs.module';
 import { JobsMiddlewareModule } from './jobs-middleware/jobs-middleware.module';
+import { JobsInterceptorModule } from './jobs-interceptor/jobs-interceptor.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [JobsModule, JobsMiddlewareModule],
+  imports: [JobsModule, JobsMiddlewareModule, JobsInterceptorModule, AuthModule, UsersModule],
   controllers: [],
   providers: [],
 })
